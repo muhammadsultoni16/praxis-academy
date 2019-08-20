7a0de03639618cf0633759302807a3f863157c08
muhammadsultoni16 <muhammadsultoni.sulton16@gmail.com>
2019-08-20 23:45:46 +0700

    added matrixs

--- novice/01-01/kasus/matrixinput.java
+++ novice/01-01/kasus/matrixinput.java
@@ -11,28 +11,36 @@ public class matrixinput {
         int kolom = scan.nextInt();
 
         int[][] matrixinput = new int [baris][kolom];
+        int[][] matrixinput2 = new int [baris][kolom];
 
-        enterMatrixData(scan, matrixinput, baris, kolom);
+        enterMatrixData(scan, matrixinput, matrixinput2, baris, kolom);
 
-        hasil(matrixinput, baris, kolom);
+        hasil(matrixinput, matrixinput2, baris, kolom);
     }
-        public static void enterMatrixData(Scanner scan , int[][] matrixinput, int baris, int kolom) {
-            System.out.println("Masukan Data Matrix");
+        public static void enterMatrixData(Scanner scan , int[][] matrixinput, int[][] matrixinput2, int baris, int kolom) {
+            System.out.println("Masukan Data Matrix 1");
             for (int i = 0; i < baris; i++){
                 for (int j = 0; j < kolom; j++){
                     matrixinput[i][j] = scan.nextInt();
                 }
             }
-            
-           
+            System.out.println("Masukan Data Matrix 2");
+                for (int i = 0; i < baris; i++){
+                    for (int j = 0; j < kolom; j++){
+                        matrixinput2[i][j] = scan.nextInt();
+                    }
+                }
+        }    
                 
-            } 
-            public static void hasil(int [][] matrixinput, int baris, int kolom) {
+                
+ 
+            public static void hasil(int[][] matrixinput,int[][] matrixinput2, int baris, int kolom) {
                 System.out.println("matriks");
-
+                int[][] hasil = new int[baris][kolom];
                 for (int i= 0; i < baris; i++){
                     for (int j= 0; j < kolom; j++){
-                        System.out.print(matrixinput[i][j]+"\t");
+                        hasil[i][j] = matrixinput[i][j]+matrixinput2[i][j];
+                        System.out.print(hasil[i][j]+"\t");
 
                     }
 
